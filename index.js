a2d3fb4cbdb020c9600f50d3588bb957fc3fe8b4
david <studen.david@gmail.com>
2021-04-29 11:30:58 +0200

    das

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const path = require('path');
 const app = express();
 require('dotenv').config();
 
-const port = process.env.EXPRESS_PORT;
-
 app.use(express.static('client/public'))
 app.get("/", (req, res) => {
     res.sendFile( path.resolve(__dirname, "client", "public", "index.html"));
 })
 
-app.listen(port||5000, err => {
+app.listen(process.env.PORT||3000, err => {
     if(err) console.log(err);
     console.log(`Server listening on port ${port}`);
 })
